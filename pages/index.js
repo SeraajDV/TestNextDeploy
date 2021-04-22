1c02d2171d32444d17598cf5c6d0b14badb10282
Seraaj <seraajdevilliers@gmail.com>
2021-04-22 14:49:33 +0200

    a

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,40 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+//Import the Link API to support client-side navigation
+import Link from 'next/link'
+import Layout from '../components/MyLayout'
+import Image from 'react-bootstrap/Image'
 
-export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
-}
+export default () => (
+	<div>
+	<Layout>
+	    <h2>Welcome to my first dev porti!</h2>
+	    <Image className="banner_Img" src="/static/headerimage.jpeg" alt="My image"/>
+		<style jsx>{`
+			h2 {
+				margin-top: 25px;
+				font-family: 'Roboto', 'sans-serif';
+			}
+			.banner_Img {
+				margin: none;
+				border: none;
+				width: 100%;
+			}
+			`}
+		</style>
+		<style global jsx>{`
+			body {
+				text-align: center;
+			}
+			h2 {
+				margin-top: 25px;
+				margin-bottom: 25px;
+			}
+			.banner_Img {
+				margin: none;
+				border: none;
+				width: 100%;
+			}
+			`}
+		</style>
+  </Layout>
+  </div>
+)
